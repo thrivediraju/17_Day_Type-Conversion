92af3f46703247be22a8b67065b5981db39afe5c
thrivediraju <104252421+thrivediraju@users.noreply.github.com>
2022-05-17 10:41:19 +0530

    Add files via upload

---
+++ string_concatenation.py
@@ -0,0 +1,40 @@
+# a='hi' ;  b='py' ; c=2 ; d=5.6
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+
+# print(a+c) # TypeError
+# print(a+d) # TypeError
+# print(a+b) # hipy
+
+# a='hi' ;  b='py' ; c=2 ; d=5.6
+# print(a,c)  # hi 2
+# print(b,c)  # py 2
+# print(a,b)  # hi py
+# print(a,d)  # hi 5.6
+
+# print(type(''))
+
+# print(''.isspace())
+# print('b'.isspace())
+# print(' '.isspace())
+# print('    '.isspace())
+
+
+# print('h'  'i')  # hi
+# print('h '  'i')  # h i
+# print('h'  ' i')  # h i
+# print('h' , 'i')  # h i
+
+# print('h'+'i')  # hi
+# print('h'+''+'i')  # hi
+# print('h'+' '+'i')  # h i
+# print('h'+'    '+'i')  # h   i
+
+# print('py'+'    '+'3')
+
+# print('Python','3.10.4')
+# print('Python'+' '+'3.10.4')
+
+# print('TATA'+'-'+'IPL')
